d8f33f9ac1b2c67ee629034838bfaec029e5d9ab
Sam Oberly <soberly1@jh.edu>
2022-11-07 02:02:12 +0000

    changed to college

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <nav>
       <img src={require("../images/logo.png")} className="nav--logo" />
-      <h1>my travel journal.</h1>
+      <h1>my college journal.</h1>
     </nav>
   );
 }
